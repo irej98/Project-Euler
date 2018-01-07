c44c65597289885b822790da086072f1508e1a51
Jeri <35116106+irej98@users.noreply.github.com>
2018-01-07 14:06:30 +0000

    Create 001.cpp

---
+++ Solutions/001/001.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+int main()
+{
+  int total = 0;
+  for(int i=0; i<1000; i+=1)
+  {
+    if(i%3==0)
+    {
+      total += i;
+    }
+    else if(i%5==0)
+    {
+      total += i;
+    };
+  };
+  std::cout << total << std::endl;
+};
